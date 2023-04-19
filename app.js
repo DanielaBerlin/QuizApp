6dbd1dd701e89d531781f61d959c1190c1457508
Daniela <danielacardona2203@gmail.com>
2023-04-19 20:27:42 +0200

    added display question function

--- app.js
+++ app.js
@@ -1,36 +1,55 @@
 //CREATE A QUIZ CLASS
 class Quiz {
-    constructor(questions) {
-        this.score = 0;
-        this.questions = questions;
-        this.questionIndex = 0;
-    }
+  constructor(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.questionIndex = 0;
+  }
 
-    getQuestionIndex() {
-        return this.questions[this.getQuestionIndex];
+  getQuestionIndex() {
+    return this.questions[this.getQuestionIndex];
+  }
+  guess(answer) {
+    if (this.getQuestionIndex().isCorrectAnswer(answer)) {
+      this.score++;
     }
-    guess(answer) {
-        if (this.getQuestionIndex().isCorrectAnswer(answer)) {
-            this.score++;
-        }
-        this.questionIndex++;
-    }
+    this.questionIndex++;
+  }
 
-    isEnded() {
-        return this.questionIndex === this.questions.length;
-    }
+  isEnded() {
+    return this.questionIndex === this.questions.length;
+  }
 }
 
 //QUESTION CLASS
 class question {
-    constructor(text, choices, answer) {
-        this.text = text;
-        this.choices = choices;
-        this.answer = answer;
-    }
+  constructor(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+  }
 
-    isCorrectAnswer(choice) {
-        return this.answer === choice;
-    }
+  isCorrectAnswer(choice) {
+    return this.answer === choice;
+  }
+}
 
+//DISPLAY QUESTION
+
+function displayQuestion() {
+  if (Quiz.isEnded()) {
+    showScores();
+  } else {
+    //show question
+    let questionElement = document.getElementById('question');
+    questionElement.innerHTML = quiz.getQuestionIndex().text;
+
+    //show options
+    let choices = quiz.getQuestionIndex().choices;
+    for (let i = 0; i < choices.length; i++) {
+      let choiceElement = document.getElementById('choice' + i);
+      choiceElement.innerHTML = choices[i];
+      guess('btn' + i, choices[i]);
+    }
+  }
 }

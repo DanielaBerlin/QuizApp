d1fe7945c3b6e64c2a3d8b8f001eebc67461a5e6
Daniela <danielacardona2203@gmail.com>
2023-04-19 22:07:13 +0200

    added showScores function to app.js

--- app.js
+++ app.js
@@ -75,3 +75,16 @@ function showProgress() {
     `Question ${currentQuestionNumber} of ${quiz.question.length}`;
 }
 
+//SHOW SCORE
+function showScores() {
+    let quizEndHTML = 
+    `
+        <h1>Quiz Completed</h1> 
+        <h2 id="score">You Scored: ${quiz.score} of ${quiz.question.length} </h2>
+        <div class="quiz-repeat">
+            <a href="index.html">Take Quiz Again</a>
+        </div>
+    `;
+    let quizElement = document.getElementById("quiz");
+    quizElement.innerHTML = quizEndHTML;
+}

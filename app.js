01b3aa8e34fa6f44ba997a10e212197997673338
Daniela <danielacardona2203@gmail.com>
2023-04-19 20:14:11 +0200

    created a Quiz class with questions, undex and answer methods

---
+++ app.js
@@ -0,0 +1,19 @@
+//CREATE A QUIZ CLASS
+class Quiz {
+    constructor(questions) {
+        this.score = 0;
+        this.questions = questions;
+        this.questionIndex = 0;
+    }
+
+    getQuestionIndex() {
+        return this.questions[this.getQuestionIndex];
+    }
+    guess(answer) {
+        if (this.getQuestionIndex().isCorrectAnswer(answer)) {
+            this.score++;
+        }
+        this.questionIndex++;
+    }
+    
+}

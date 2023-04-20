6795fcdf82b4c9586546a4357e254eea9633e774
Daniela <danielacardona2203@gmail.com>
2023-04-20 22:59:44 +0200

    added startCountDown function to set up a timer on the quiz

--- app.js
+++ app.js
@@ -120,3 +120,27 @@ let quiz = new Quiz(questions);
 
 //display question
 displayQuestion();
+
+//ADD A COUNTDOWN
+
+let time = 10;
+let quizTimeInMinutes = time * 60 * 60;
+quizTime = quizTimeInMinutes / 60;
+
+let counting = document.getElementById("count-down");
+
+function startCountdown() {
+    let quizTimer = setInterval(function() {
+        if (quizTime <= 0) {
+            clearInterval(quizTimer);
+            showScores();
+        } else {
+        quizTime--;
+        let sec = Math.floor(quizTime % 60);
+        let min = Math.floor(quizTime / 60) % 60;
+        counting.innerHTML = `TIME: ${min} : ${sec}`;
+        }
+    }, 1000)
+}
+
+startCountdown();
